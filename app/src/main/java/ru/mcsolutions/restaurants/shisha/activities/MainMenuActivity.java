89f1f939bc52e5d1d73a3697b38f063cc2129494
iatru <i.a.trubnikov@gmail.com>
2016-11-16 16:48:42 +0300

    Заготовка для фабов готова к работе

--- app/src/main/java/ru/mcsolutions/restaurants/shisha/activities/MainMenuActivity.java
+++ app/src/main/java/ru/mcsolutions/restaurants/shisha/activities/MainMenuActivity.java
@@ -2,10 +2,21 @@ package ru.mcsolutions.restaurants.shisha.activities;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionMenu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ru.mcsolutions.restaurants.shisha.R;
 import ru.mcsolutions.restaurants.shisha.adapters.MainMenusRecyclerAdapter;
@@ -16,10 +27,20 @@ public class MainMenuActivity extends AppCompatActivity {
     Context context = this;
     TextView textViewLocation;
 
+
+    private FloatingActionMenu menuBlue;
+    private com.github.clans.fab.FloatingActionButton fab1;
+    private com.github.clans.fab.FloatingActionButton fab2;
+
+    private com.github.clans.fab.FloatingActionButton fabEdit;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        menuBlue = (FloatingActionMenu) findViewById(R.id.menu_blue);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
@@ -29,5 +50,28 @@ public class MainMenuActivity extends AppCompatActivity {
 
         textViewLocation = (TextView) findViewById(R.id.textViewLocation);
         textViewLocation.setText(Global.location.getName());
+
+        menuBlue.setIconAnimated(true);
+        menuBlue.setClosedOnTouchOutside(true);
+        fab1 = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.fab1);
+        fab2 = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.fab2);
+
+        fab1.setOnClickListener(clickListener);
+        fab2.setOnClickListener(clickListener);
+
     }
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.fab1:
+                    Toast.makeText(context, "Нажат фаб 1", Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.fab2:
+                    Toast.makeText(context, "Нажат фаб 2", Toast.LENGTH_LONG).show();
+                    break;
+
+            }
+        }
+    };
 }
